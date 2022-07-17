ea3f1156b810d147b4e75fe9f28dc66189de0c0f
zhuoran <ranoriginals@gmail.com>
2022-07-17 11:29:05 -0400

    Update tstar_parameters.py

--- tstar_parameters.py
+++ tstar_parameters.py
@@ -9,20 +9,29 @@ def set_parameters():
     Parameters used in frequency and time domains, modify them to fit your dataset.
 
     :param snrcrtp1: Only matters when source_para = 1.
-        Minimum [SNR, frequency band width???] for finding best fc and alpha.
 
     :param dstress:
-        Stress drop in [MPa], for calculating the reasonal range of theoretical corner frequencies.
+        Stress drop in [MPa], for calculating the reasonable range of theoretical corner frequencies.
 
     :param doplotseis: bool
-        Determine whether to plot all the three-component seismograms in ./wordir/specfig/plotseis.
+        Determine whether to plot all the three-component seismograms in ./workdir/specfig/plotseis.
     :param doplotsnr: bool
+        Determine whether to plot all the SNR of spectra in ./workdir/specfig/plotsnr.
+    :param doplotspec: bool
+        Determine whether to plot all the spectra in log(Am)-linear(Fre) scale in ./workdir/specfig/plotspec.
+    :param plotspecloglog: bool
+        Determine whether to plot all the spectra in log(Am)-log(Fre) scale in ./workdir/specfig/plotspec.
+    :param doplotfcts: bool
+        Determine whether to plot t* perturbation versus corner frequency in ./workdir/specfig/plottstar-fc.
+    :param doplotfcall: bool
+        Determine whether to plot L2 Norm and Seismic Moment versus corner frequency in ./workdir/specfig/plotfcall.
 
     :param source_para:
         The option for determining the source spectra-
-            1: grid search for fc and Mw to calculate the source spectra
-            2: input fc and Mw to caulcte the source spectra
+            1: grid search for fc and Mw to calculate the source spectra.
+            2: input fc and Mw to caulcte the source spectra.
             3: input source spectra directly.
+
     :param input_arriv:
         The option to read P and S arrival times-
             0: read arrival time from sac files
@@ -35,40 +44,37 @@ def set_parameters():
         # "npol": 4,
         "frminp": 4,
 
-        "snrcrtp1": [3.0,4.0,3.0],   ## MINIMUM [SNR,FREQUENCY BAND WIDTH] FOR FINDING BEST fc AND alpha
-        ## see the hints for the second and third parameter in longseg
-        "snrcrtp2": [2.0,3.0,2.0],    ## MINIMUM [SNR,FREQUENCY BAND WIDTH] FOR t* INVERSION
-        "snrcrts1": [2.5,2.0,1.2],    ## MINIMUM [SNR,FREQUENCY BAND WIDTH] FOR FINDING BEST fc AND alpha
-        "snrcrts2": [1.8,1.0,1.2],    ## MINIMUM [SNR,FREQUENCY BAND WIDTH] FOR t* INVERSION
-        "lincor": [0.7,0.7,0.7],    ## MINIMUM LINEAR CORRELATION COEFFICIENTS
-        "misfitP": 0.85,    ## MINIMUM MISFIT FOR P WAVE MEASURED BY CORRELATIVE FUNCTION
-        "misfitP2": 0.2,    ## MAXIMUM L2 NORM FOR P WAVE
-        "misfitS": 0.85,    ## MINIMUM MISFIT FOR S WAVE MEASURED BY CORRELATIVE FUNCTION
-        "misfitS2": 0.2,    ## MAXIMUM L2 NORM FOR S WAVE
+        "snrcrtp1": [3.0,4.0,3.0],  ## Minimum [SNR, frequency band width] for finding best fc and alpha
+                                    ## See the hints for the second and third parameter in tstarsub.longseg
+        "snrcrtp2": [2.0,3.0,2.0],  ## Minimum [SNR, frequency band width] for t* inversion
+        "snrcrts1": [2.5,2.0,1.2],  ## Minimum [SNR, frequency band width] for finding best fc and alpha
+        "snrcrts2": [1.8,1.0,1.2],  ## Minimum [SNR, frequency band width] for t* inversion
+        "lincor": [0.7,0.7,0.7],    ## Minimum linear correlation coefficients
+        "misfitP": 0.85,            ## Minimum misfit for P wave measured by correlative function
+        "misfitP2": 0.2,            ## Maximum L2 Norm for P wave
+        "misfitS": 0.85,            ## Minimum misfit for S wave measured by correlative function
+        "misfitS2": 0.2,            ## Maximum L2 Norm for S wave
 
-        "dstress": [0.5,20.0],   
-        "Re": 6371,
+        "dstress": [0.5,20.0],
 
-        ##TIME DOMAIN PARAMETERS
-        "WLP": 5.0,
-        "WLS": 8.0,
-        "pretime": 50,
-        "posttime": 150,
-        "prewin": [0.5,1],  ## WINDOW START BEFORE ARRIVAL
-        "gaps": 0.5,        ## seconds between S noise and signal
-        "dt": 0.025,
-        "beta": 4000,       ## SHEAR VELOCITY IN m/s FOR APPROXIMATING CORNER FREQUENCY
+        ## TIME DOMAIN PARAMETERS
+        "WLP": 5.0,         ## Window length of P wave signal and corresponding noise
+        "WLS": 8.0,         ## Window length of S wave signal and corresponding noise
+        #"pretime": 50,
+        "prewin": [0.5,1],  ## Window start before arrival
+        "gaps": 0.5,        ## Seconds between S noise and signal
+        "beta": 4000,       ## Shear velocity in m/s for approximating corner frequency
 
-        ##OPTIONS FOR PLOTTING FIGURES
+        ## OPTIONS FOR PLOTTING FIGURES
         "doplotseis": False,
         "doplotsnr": False,
         "doplotspec": False,
-        "plotspecloglog": True,
+        "plotspecloglog": False,
         "doplotfcts": True,
         "doplotfcall": True,
 
-        ##OPTIONS FOR DETERMINING SOURCE PARAMTERS
-        "source_para": 2,
+        ## OPTIONS FOR DETERMINING SOURCE PARAMTERS
+        "source_para": 1,
         "input_arriv": 1, 
         "alpha": 0.27,
         "fcps": 1.0,       ## fc(P)/fc(S)
@@ -77,25 +83,19 @@ def set_parameters():
     return param
 
 def working_dir():
-    """ All the working directories with absolute paths
+    """ 
+    Define all the working dictionaries with absolute paths and return the event list
     """
     global workdir, sacdir, gsdir, figdir, resultdir, figdir1, figdir2, figdir3, figdir4, figdir5, logfl, fclist
-    maindir = os.path.abspath(os.getcwd())
-    # workdir = maindir + '/workdir'    ## OUTPUT DIRECTOR
     
-    # need to modify! (please use: workdir = maindir + '/workdir')
-    workdir = '/mnt/gs21/scratch/yurong/tstar/workdir' # OUTPUT to scratch dorectory on hpcc when running with a large dataset
-    # need to modify! (please use: workdir = maindir + '/workdir')
-    if not os.path.isdir(workdir):
-        os.mkdir(workdir)   
-
+    maindir = os.getcwd()
+    sacdir = maindir + '/data/processedSeismograms'
     namedir = maindir + '/data/Eventname'
-    # sacdir = maindir + '/data/processedSeismograms'
-    # need to modify! (please use: sacdir = maindir + '/data/processedSeismograms')
-    sacdir = '/mnt/home/yurong/Research/tstar/4YZhang/sacfl'
-    # need to modify! (please use: sacdir = maindir + '/data/processedSeismograms')
-
     gsdir = maindir + '/data/GS'
+
+    workdir = maindir+'/workdir'  ## Output dictionary
+    if not os.path.exists(workdir):
+        os.makedirs(workdir)
     
     resultdir = workdir + '/result' 
     figdir  = workdir + '/specfig'
@@ -105,21 +105,21 @@ def working_dir():
     figdir4 = figdir + '/plottstar-fc'
     figdir5 = figdir + '/plotfall'
 
-    logfile = workdir + '/event.log' 
-    logfl=open(logfile,'a')
-    fclist=open(workdir + '/bestfc.lst','a')
-    
-
     dir_lst = [figdir, resultdir, figdir1, figdir2, figdir3, figdir4, figdir5]
     for idir in dir_lst:
-        if not os.path.isdir(idir):
-            os.mkdir(idir)
+        if not os.path.exists(idir):
+            os.mkdirs(idir)
+
+    logfile = workdir + '/event.log' 
+    logfl = open(logfile,'w')
+    fclist = open(workdir + '/bestfc.lst','w')
     
-    oridlst=[]
-    for name in open(namedir).readlines():
+    oridlst = []
+    forid = open(namedir, 'r')
+    for name in forid.readlines():
         name = name.strip('\n')
         if name is not None:
             oridlst.append(name)
+    forid.close()
 
     return oridlst
-
